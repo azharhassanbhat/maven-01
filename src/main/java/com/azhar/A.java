ddc81a9501cb8774810847676fb5653d0dd87626
azharhassanbhat <azharhassanbhat@gmail.com>
2024-11-27 15:00:10 +0530

    modified -122

--- src/main/java/com/azhar/A.java
+++ src/main/java/com/azhar/A.java
@@ -14,6 +14,8 @@ import java.util.stream.IntStream;
 public class A {
 
 	public static void main(String[] args) {
+		System.out.println(1000);
+		System.out.println(2000);
 	String s="hello";
 	String rev = reverseString(s);
 	System.out.println(rev);
